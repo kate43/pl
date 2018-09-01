94525f2c7707e7dacc1d8384fa1feebb2f1b7f64
Jinhyuk Jeon <danthefreewizard@gmail.com>
2018-09-01 11:41:13 +0900

    4-1

--- hw1.ml
+++ hw1.ml
@@ -1,3 +1,19 @@
+type nat = ZERO | SUCC of nat
+
+let rec natadd (a,b) =
+	match(a,b) with
+		| (ZERO,ZERO) -> ZERO
+		| (ZERO, b) -> b
+		| (a, ZERO) -> a
+		| (SUCC a', SUCC b') -> SUCC(natadd(a,b'))
+	
+(*		| (a,SUCC b`)-> natadd(a,b');
+				   SUCC a
+		| (SUCC a', b) -> natadd(a',b); SUCC b
+*)
+
+let zet1 = SUCC(ZERO)
+let _ = natadd(zet1,zet1)
 
 let rec merge (l1,l2) = 
 	match (l1,l2) with
@@ -14,6 +30,15 @@ let rec printer msg =
 			print_int h;
 			printer t
 
-let greeter = merge([1;2;3],[4;5;6])
+(*let greeter = merge([1;2;3],[4;5;6])
 
 let _ = printer greeter
+*)
+
+let rec sigma(a,b,f) =
+	if a>b-1 then 0
+	else f(b) + sigma(a,b-1,f)
+
+let _ = print_int(sigma(0,5,fun x->x+3))
+
+
